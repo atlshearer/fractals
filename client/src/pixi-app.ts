58cb3e62be596fac050a0f4693caa520a6c8e776
Alexander Shearer <alexshearer00@gmail.com>
2022-09-25 06:59:52 +0000

    General clean up

--- client/src/pixi-app.ts
+++ client/src/pixi-app.ts
@@ -1,9 +1,9 @@
-import { Application, Container, Filter, Graphics, Matrix, Program, settings, Shader, Sprite } from "pixi.js";
+import { Application, Filter, Graphics, settings } from "pixi.js";
 
 const VERT_SRC = `
 #version 300 es
-precision highp float;
-precision highp int;
+precision mediump float;
+
 in vec2 aVertexPosition;
 
 uniform mat3 projectionMatrix;
@@ -16,7 +16,7 @@ uniform vec4 outputFrame;
  
 vec4 filterVertexPosition( void )
 {
-   vec2 position = aVertexPosition * max(outputFrame.zw, vec2(0.0f)) + outputFrame.xy;
+    vec2 position = aVertexPosition * max(outputFrame.zw, vec2(0.0f)) + outputFrame.xy;
 
     return vec4((projectionMatrix * vec3(position, 1.0f)).xy, 0.0f, 1.0f);
 }
@@ -36,8 +36,8 @@ void main(void)
 
 const FRAG_SRC = `
 #version 300 es
-precision highp float;
-precision highp int;
+precision mediump float;
+
 in vec2 vTextureCoord;
 in vec2 vFilterCoord;
 out vec4 fragColor;
@@ -51,36 +51,33 @@ uniform float scale;
 
 int get_iterations()
 {
-   float real = (vFilterCoord.x - 0.5f) * scale * aspect + centerX;
-   float imag = (vFilterCoord.y - 0.5f) * scale + centerY;
+    float real = (vFilterCoord.x - 0.5f) * scale * aspect + centerX;
+    float imag = (vFilterCoord.y - 0.5f) * scale + centerY;
  
     int iterations = 0;
-   float const_real = real;
-   float const_imag = imag;
+    float const_real = real;
+    float const_imag = imag;
  
     for (int j = 0; j < MAX_ITERATIONS; j += 1)
     {
-       float tmp_real = real;
+        float tmp_real = real;
         real = (real * real - imag * imag) + const_real;
         imag = (2.0 * tmp_real * imag) + const_imag;
          
-       float dist = real * real + imag * imag;
+        float dist = real * real + imag * imag;
          
         if (dist > 4.0)
-        break;
+          break;
  
         ++iterations;
     }
+
     return iterations;
 }
  
 vec4 return_color()
 {
     int iter = get_iterations();
-    // if (iter == MAX_ITERATIONS)
-    // {
-    //     return vec4(0.0, 0.0, 0.0, 1.0);
-    // }
  
     float iterations = float(iter) / float(MAX_ITERATIONS);    
     return vec4(iterations, iterations, iterations, 1.0);
@@ -88,31 +85,7 @@ vec4 return_color()
  
 void main()
 {
-    // gl_FragColor = vec4(vTextureCoord.x, vTextureCoord.y, 0.0, 1.0);
     fragColor = return_color();
-    
-    // float real = (vTextureCoord.x * 4.0 - 2.0 + centerX) * aspect;
-    // float imag = (vTextureCoord.y * 4.0 - 2.0 + 0.0);
-
-    // if (real * real + imag * imag < 1.0) {
-    //     gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-    // } else {
-    //     gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
-    // }
-
-    // if (vFilterCoord.x > 0.6) {
-    //   gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
-    // } else {
-    //   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-    // }
-
-    
-    // if (vFilterCoord.y > 0.9) {
-    //   gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
-    // }       
-
-    // gl_FragColor = vec4(vTextureCoord.x, 0.0, 0.0, 1.0);
-
 }`;
 
 export const setupPixi = () => {
@@ -128,35 +101,26 @@ export const setupPixi = () => {
 
   app.loader.load(startup);
 
-
   function startup() {
     const width = app.screen.width;
     const height = app.screen.height;
 
     const uniform = {
-      scale: 0.000010173536293987636,
-      centerX: 0.3515400588615876,
-      centerY: -0.5067588609443336,
-      // aspect: 1.0,
+      scale: 2.0,
+      centerX: 0.0,
+      centerY: 0.0,
       aspect: width / height,
     };
-
 
     const filter = new Filter(VERT_SRC, FRAG_SRC, uniform);
 
     filter.resolution = 0.5;
-    console.log("RES", filter.resolution)
-
-    console.log(app.screen.width, app.screen.height, app.screen.width / app.screen.height);
 
     const graphics = new Graphics();
-
-    console.log(height)
 
     graphics.beginFill(0xffffff);
     graphics.drawRect(0, 0, width, height);
     graphics.endFill();
-
 
     graphics.filters = [filter];
 
